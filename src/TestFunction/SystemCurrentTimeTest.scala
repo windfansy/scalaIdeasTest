3c2a4d8e7bff91bf8999b420b3a7a8ede05eda1f
chenxirong <chenxirong@gridsum.com>
2016-05-04 10:24:23 +0800

    add init date by long time

---
+++ src/TestFunction/SystemCurrentTimeTest.scala
@@ -0,0 +1,12 @@
+package TestFunction
+
+import java.util.Date
+
+/**
+  * Created by T440P on 2016/5/4.
+  */
+object SystemCurrentTimeTest {
+  def main(args: Array[String]) {
+    println(new Date(1349374400000L))
+  }
+}
