aa37bfdcd6ce175ae68a863ab8667fe748a2cca5
windfansy <windfansy@126.com>
2016-05-12 23:29:07 +0800

    add DateFormat Test

---
+++ src/TestFunction/DateFormatTest.scala
@@ -0,0 +1,17 @@
+package TestFunction
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+/**
+  * Created by windfansy on 2016/5/12.
+  */
+object DateFormatTest {
+  def main(args: Array[String]) {
+    val allDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+    val timeFormat = new SimpleDateFormat("HH:mm")
+    println(allDateFormat.format(new Date(1146656000000L)))
+    println(timeFormat.format(new Date(1146656000000L)))
+  }
+
+}
