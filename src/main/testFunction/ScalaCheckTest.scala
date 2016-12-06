374343cd9dc86124b5c6ab3d767c95246e4a038b
chenxirong <chenxirong@gridsum.com>
2016-12-06 20:56:15 +0800

    save 2016-12-06:12:56:15

--- src/main/testFunction/ScalaCheckTest.scala
+++ src/main/testFunction/ScalaCheckTest.scala
@@ -7,13 +7,16 @@ import org.scalacheck.Gen
   */
 object ScalaCheckTest {
   def main(args: Array[String]) {
-    val stringGen = for {
-      n <- Gen.choose(0, 20)
-    } yield (Random.alphanumeric take n) mkString
-//      yield
+//    val stringGen = for {
+//      n <- Gen.choose(0, 20)
+//    } yield (Random.alphanumeric take n) mkString
+////      yield
+//
+//    println(stringGen)
+//    println(stringGen.sample.get)
+//    println((Random.alphanumeric take 10).mkString)
 
-    println(stringGen)
-    println(stringGen.sample.get)
-    println((Random.alphanumeric take 10).mkString)
+   println("9fd3c359-d7ad-4bee-beb5-6717fee58a9e".hashCode.toString.last)
+   println(s"${"81aec5f3-5c21-4849-aa48-0c54dea9d18d".hashCode.toString.last}${2915}${"@"}${"81aec5f3-5c21-4849-aa48-0c54dea9d18d"}")
   }
 }
