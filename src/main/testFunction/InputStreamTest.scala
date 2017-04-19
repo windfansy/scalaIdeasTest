f13e2f61323910cb2ee96977749d69129e13e5fc
chenxirong <chenxirong@gridsum.com>
2017-04-19 19:28:39 +0800

    save 2017-04-19:11:28:39

--- src/main/testFunction/InputStreamTest.scala
+++ src/main/testFunction/InputStreamTest.scala
@@ -1,3 +1,5 @@
+import java.net.URI
+
 import scala.io.Source
 
 /**
@@ -5,11 +7,16 @@ import scala.io.Source
   */
 object InputStreamTest {
   def main(args: Array[String]) {
-    val inputStream = this.getClass.getClassLoader.getResourceAsStream("dimIntervals.json")
-    if (inputStream != null) {
-      val source = Source.fromInputStream(inputStream)("UTF-8")
-      val jsonString = source.getLines() mkString
-    }
+//    val inputStream = this.getClass.getClassLoader.getResourceAsStream("dimIntervals.json")
+//    if (inputStream != null) {
+//      val source = Source.fromInputStream(inputStream)("UTF-8")
+//      val jsonString = source.getLines() mkString
+//    }
+    val url = "http://wd3.gridsumdissector.com/#/reportCenter?sid=5015&pid=5069&paramsid=838c41811926b2b07548c14c89bffdfd&ud=1492511427534"
+    val trimedUrl = url.trim
+    val uri = new URI(trimedUrl).normalize()
+    println(uri.getRawQuery)
+    println(uri.getRawFragment)
 
 
   }
