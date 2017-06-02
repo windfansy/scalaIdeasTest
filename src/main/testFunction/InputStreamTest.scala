7924d0831e8e046e656bac6ae147f9d60b80a1de
chenxirong <chenxirong@gridsum.com>
2017-06-02 18:07:22 +0800

    save 2017-06-02:10:07:22

--- src/main/testFunction/InputStreamTest.scala
+++ src/main/testFunction/InputStreamTest.scala
@@ -1,7 +1,7 @@
 import java.net.URI
 import java.util.{Date, Calendar}
 
-import scala.io.Source
+
 
 /**
   * Created by T440P on 2016/6/27.
@@ -18,12 +18,26 @@ object InputStreamTest {
 //    val uri = new URI(trimedUrl).normalize()
 //    println(uri.getRawQuery)
 //    println(uri.getRawFragment)
-    val CALENDAR = Calendar.getInstance()
-    CALENDAR.setTime(new Date(116, 0, 2))
-    CALENDAR.setMinimalDaysInFirstWeek(7)
-    println(CALENDAR.get(Calendar.WEEK_OF_YEAR))
+//    val CALENDAR = Calendar.getInstance()
+//    CALENDAR.setTime(new Date(116, 0, 2))
+//    CALENDAR.setMinimalDaysInFirstWeek(7)
+//    println(CALENDAR.get(Calendar.WEEK_OF_YEAR))
+    val e = new Exception("123")
+    if (e.isInstanceOf[RuntimeException]) {
+      System.out.println("Exception goes beyond the capability of log parser. Will rethrow it.", e)
+      throw e
+    } else
+      System.out.println("not")
 
 
   }
+
+  class AppNameResolveException(appKey:String) extends NullPointerException{
+    override def getMessage:String ={
+      s"AppName Can't be resolved for app key: $appKey"
+    }
+  }
+
+
 
 }
